1a46991ed9b3ab698e871f363dc21a6e560ac706
darkmount <me@heywesty.com>
2020-02-18 13:15:12 +0100

    update app to use routing

--- interfaces/Griffin/src/components/App/App.js
+++ interfaces/Griffin/src/components/App/App.js
@@ -1,10 +1,42 @@
-import React from "react";
-import { Button } from "../Button";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { ErrorBoundary } from "../ErrorBoundary";
+import { GlobalStyles } from "../../styles/global.styles";
+
+const Home = lazy(() => import("../../pages/Home"));
+const Queue = lazy(() => import("../../pages/Queue"));
+const History = lazy(() => import("../../pages/History"));
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="./">Home</Link>
+      </li>
+      <li>
+        <Link to="./queue">Queue</Link>
+      </li>
+      <li>
+        <Link to="./history">History</Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export const App = props => {
   return (
-    <>
-      <Button>Hello world</Button>
-    </>
+    <Router basename={"/sabnzbd"}>
+      <ErrorBoundary>
+        <Nav />
+        <GlobalStyles />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/history" component={History} />
+            <Route path="/queue" component={Queue} />
+          </Switch>
+        </Suspense>
+      </ErrorBoundary>
+    </Router>
   );
 };
